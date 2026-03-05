e3d00486d700b8f4aa241f8d1ba12658a90209fd
Xiaochen Du <dux@outlook.sg>
2026-03-05 13:10:12 -0500

    Add conda environment configuration and update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import setup, find_packages
-    
+from setuptools import find_packages, setup
+
 # Version Number:
 version = '0.0.1'
 
@@ -10,9 +10,14 @@ setup(
     description="Diffusion structure search",
     install_requires=[
         "numpy",
+        "pyyaml",
         "torch",
         "tensorboard",
-        "schnetpack"
+        "schnetpack",
+        "matplotlib",
+        "pandas",
+        "scipy",
+        "seaborn",
     ],
     python_requires=">=3.5",
     packages=find_packages(),
